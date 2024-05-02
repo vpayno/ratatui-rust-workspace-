7ad337b54c2c5907931d5f7a9c887d13c8d89bcc
Victor Payno <vpayno@users.noreply.github.com>
2024-05-01 18:56:13 -0700

    ci: add hello-desktop debian dependencies

---
+++ .github/citools/debian-install-deps.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+printf "\n"
+printf "Installing Debian 12 project dependencies:\n"
+printf "\n"
+
+declare deb_pkg
+declare -a deb_pkgs=(
+	lcov
+	build-essential curl wget libssl-dev
+	libwebkit2gtk-4.0-dev libgtk-3-dev libappindicator3-dev librsvg2-dev
+	libsoup-3.0-dev libwebkit2gtk-4.1-dev libxdo-dev
+)
+
+# to reduce resolution conflicts, installing one at a time
+time for deb_pkg in "${deb_pkgs[@]}"; do
+	sudo apt install -y "${deb_pkg}"
+done
